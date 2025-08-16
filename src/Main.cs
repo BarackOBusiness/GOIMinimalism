92a5984b5b9dcc9fc99510d31f72224d8997e425
BarackOBusiness <nichjoh21@gmail.com>
2025-08-16 02:55:20 -0400

    Initial project

---
+++ src/Main.cs
@@ -0,0 +1,13 @@
+﻿using BepInEx;
+
+namespace Minimalism
+{
+    [BepInPlugin("goi.ext.minimalism", "Minimalism", "0.1.0")]
+    public class Minimalism : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            Logger.LogInfo("GOI Minimalism is ready");
+        }
+    }
+}
